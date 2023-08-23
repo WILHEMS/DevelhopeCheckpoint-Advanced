60701066dabfdb675ae38e92a46bd76213e2e01f
Bolton Derrick <derekwilhems@gmail.com>
2023-08-23 12:54:01 +0300

    updates

--- checkpoint/src/sqlcode/retrieveorder.sql
+++ checkpoint/src/sqlcode/retrieveorder.sql
@@ -1,4 +1,4 @@
-SELECT p.product_name, p.quantity, o.orderdate from customers c
-FROM products p
-join orders o on c.customer_id = o.customer_id
+SELECT p.product_name, p.quantity, o.orderdate
+FROM customers c
+JOIN orders o on c.customer_id = o.customer_id
 WHERE customer_id = 1
