559cfd8ef146d53c6d17e072042c75d178b72b7d
Bolton Derrick <derekwilhems@gmail.com>
2023-08-23 12:35:10 +0300

    updates

--- checkpoint/src/onlineretail/ClothingProduct.java
+++ checkpoint/src/onlineretail/ClothingProduct.java
@@ -1,7 +1,13 @@
 package onlineretail;
 
 public class ClothingProduct extends Product{
+    public String clothMaterial;
     public ClothingProduct(String name, double price, Integer quantity) {
         super(name, price, quantity);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
     }
 }
