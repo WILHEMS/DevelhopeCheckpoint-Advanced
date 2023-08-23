f809d2e90dd3378eee88c43618de0b8008ba0038
Bolton Derrick <derekwilhems@gmail.com>
2023-08-23 11:50:15 +0300

    updates

--- checkpoint/src/sqlcode/totalsales.sql
+++ checkpoint/src/sqlcode/totalsales.sql
@@ -1,4 +1,4 @@
-SELECT c.category_name, SUM(p.price * o.quantity) AS total_revenue
+SELECT c.category_name, SUM(p.price * o.quantity) AS total_sales
 FROM products JOIN orders o ON p.product_id = o.product_id
 JOIN categories c ON p.category_id = c.category_id
 GROUP BY c.category_name;
