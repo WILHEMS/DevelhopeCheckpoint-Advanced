42b47e4585a912c097443d1920f9c5e4463a2ca8
Bolton Derrick <derekwilhems@gmail.com>
2023-08-23 12:25:17 +0300

    updates

--- checkpoint/src/payment/CreditCardPayment.java
+++ checkpoint/src/payment/CreditCardPayment.java
@@ -1,4 +1,41 @@
 package payment;
 
-public class CreditCardPayment {
+import java.util.Objects;
+
+public class CreditCardPayment implements PaymentProcessor{
+    public String cardNumber;
+    public String name;
+    public String transactionId;
+    public double accountBalance;
+
+    public CreditCardPayment(String cardNumber, String name, String transactionId, double accountBalance, double transactionAmount) {
+        this.cardNumber = cardNumber;
+        this.name = name;
+        this.transactionId = transactionId;
+        this.accountBalance = accountBalance;
+        this.transactionAmount = transactionAmount;
+    }
+
+    public String getTransactionId() {
+        return transactionId;
+    }
+    public boolean paymentStatus(PayPalPayment payment, String transactionId, String email, double accountBalance){
+        if(Objects.equals(transactionId, payment.getTransactionId())){
+            processPayment();
+            return true;
+        }
+        refundPayment();
+        return false;
+    }
+
+    public double transactionAmount;
+    @Override
+    public void processPayment() {
+
+    }
+
+    @Override
+    public void refundPayment() {
+
+    }
 }
--- checkpoint/src/payment/PayPalPayment.java
+++ checkpoint/src/payment/PayPalPayment.java
@@ -1,5 +1,42 @@
 package payment;
 
-public class PayPalPayment {
+import java.util.Objects;
 
+public class PayPalPayment implements  PaymentProcessor{
+public String email;
+public String name;
+public String transactionId;
+public double accountBalance;
+
+public double transactionAmount;
+
+    public PayPalPayment(String email, String name, String transactionId) {
+        this.email = email;
+        this.name = name;
+        this.transactionId = transactionId;
+    }
+
+    public String getTransactionId() {
+        return transactionId;
+    }
+    public boolean paymentStatus(PayPalPayment payment, String transactionId, String email, double accountBalance){
+        if(Objects.equals(transactionId, payment.getTransactionId())){
+            processPayment();
+            return true;
+        }
+        refundPayment();
+        return false;
+    }
+    @Override
+    public void processPayment() {
+        this.transactionId = String.valueOf(0);
+        accountBalance = accountBalance - transactionAmount;
+    }
+
+    @Override
+    public void refundPayment() {
+        this.transactionId = String.valueOf(0);
+        accountBalance = accountBalance + transactionAmount;
+
+    }
 }
